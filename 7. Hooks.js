45af3edc3633caa56efb676cf1ffaf823fe0789d
EmanuelButoiGit <emanuelb1000@gmail.com>
2022-08-28 15:00:14 +0300

    Create 7. Hooks.js

---
+++ 7. Hooks.js
@@ -0,0 +1,11 @@
+// Why Use Hooks ⚓?
+// As React developers, we love breaking down complex problems into simple pieces.
+
+// Classes, however, are not simple. They:
+
+// 👉 are difficult to reuse between components
+// 👉 are tricky and time-consuming to test
+// 👉 have confused many developers and caused lots of bugs
+
+
+// Let’s get started with the State Hook:
